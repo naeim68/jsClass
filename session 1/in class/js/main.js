166c035ae68d6ce3830723e5a5d048d7adc9ad6d
naeim68 <ahmadian.naeim@gmail.com>
2021-03-07 22:05:32 +0330

    session one

---
+++ session 1/in class/js/main.js
@@ -0,0 +1,35 @@
+//  NOTE:case sentesive and follow camel case standard 
+//let firstName; 
+// Type of variable 
+/* console.log(typeof firstName);
+firstName = 12;
+console.log(typeof firstName);
+firstName = true;
+console.log(typeof firstName);
+firstName = 'naeim';
+console.log(typeof firstName);
+firstName = ['x','y','z'];
+console.log(typeof firstName);
+firstName = {'firstName':firstName,'lastName':'ahmadian','age':12,'isMarid':2};
+console.log(typeof firstName);
+*/
+
+/*
+firstName = 'naeim';
+console.log('My name is '+ firstName);
+// Add variable in string for concat with back tick best Practise
+console.log(`My name is ${firstName}`);
+*/
+
+/* Defenation Variable 
+var firstName; // Base on MDN dont use by defult 
+const lastName; // static variable for example PI number
+let age; //
+*/
+
+/*Function */
+function printHello() {
+  console.log("salam");
+}
+// Best practise call function after defenation myFunction 
+// printHello();
---
+++ session 1/training/js/main.js
@@ -0,0 +1,26 @@
+/*
+firstName = 'naeim';
+console.log('My name is '+ firstName);
+// Add variable in string for concat with back tick best Practise
+console.log(`My name is ${firstName}`);
+*/
+
+/* Defenation Variable */
+let name = 'Naeim Ahmadian';
+let age = 31;
+let job = 'System administrator';
+const nationalCode = 1230007954;
+
+function printName() {
+  console.log(`My name is ${name}.`);
+}
+function printAge() {
+  console.log(`I'm ${age} years old.`);
+}
+function printNationalCode() {
+  console.log(`It's ${nationalCode}`);
+}
+function printJob() {
+  console.log(`I'm a ${job}`);
+}
+
